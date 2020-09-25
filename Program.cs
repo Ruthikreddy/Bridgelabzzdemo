1878e4ee13bd248a3a528f88dbab30c881b94d51
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-25 23:32:56 +0530

    a.txt

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int m = 6;
+            int res = 0;
+            res = m + n;
+
+            Console.WriteLine("{0}",res);
+        }
+    }
+}
